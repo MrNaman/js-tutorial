dc69a9601ae3e2a5c259792861a65e607042fb8c
MrNaman <65702255+MrNaman@users.noreply.github.com>
2024-08-02 06:26:23 +0000

    Datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,3 +7,21 @@ console.log(3
         3) //code readability should be high 
 
 console.log("Naman")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
